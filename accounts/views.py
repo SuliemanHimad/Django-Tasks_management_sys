546767abcb3836dabcfa48430875656499d8f8ef
SuliemanHimad <suleiman.m@yooltech.com>
2025-03-22 13:27:38 +0300

    update_v2

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+
+
 
 # Signup View
 def signup_view(request):
